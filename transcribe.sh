518a565dae18cfaa4dc988a3f427893bae3b8d18
Neel Redkar <neel.redkar@gmail.com>
2024-04-04 14:14:47 -0700

    change to chunked processing for tests

--- transcribe.sh
+++ transcribe.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 ffmpeg -i $1.m4a -acodec pcm_s16le -ac 1 -ar 16000 $1.wav  
 ~/Documents/Git/whisper.cpp/main -m /Users/neelredkar/Documents/Git/whisper.cpp/models/ggml-medium.en.bin -f $1.wav -otxt -of $1
 rm $1.wav
 python transcribe.py $1.txt
 latexmk -pdf $1.tex
 latexmk -c
+mkdir $1
+mv $1*(.) $1
