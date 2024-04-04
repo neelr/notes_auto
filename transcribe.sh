5df3557bf52a0954ec199ac3b86148f5678a41b1
Neel Redkar <neel.redkar@gmail.com>
2024-04-04 15:50:02 -0700

    update transcribe script

--- transcribe.sh
+++ transcribe.sh
@@ -7,3 +7,11 @@ latexmk -pdf $1.tex
 latexmk -c
 mkdir $1
 mv $1*(.) $1
+
+# ask for name of folder to move to
+echo "Enter the name of the folder you want to move to: "
+read folder
+mv -r $1 $folder
+
+git add .
+git commit -m "Transcribed notes for $1"
